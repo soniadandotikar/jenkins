8d1ed615fe12742e120b399cbc90ef2b2837ba65
Sonia Dandotikar <73184211+soniadandotikar@users.noreply.github.com>
2023-01-16 16:02:08 +0530

    2nd commit

--- python_py.py
+++ python_py.py
@@ -13,4 +13,5 @@ square = [i**2 for i in list1]
 print('Square of the numbers are: ', square)
 cube = [i**3 for i in list1]
 print('Cube of the numbers are: ',cube)
-
+fourth_root = [i**4 for i in list1]
+print('Fourth root of the numbers are: ',fourth_root)
